9e9836f2578ed761c8d8644c40c6d7dc7cda36e9
saunmanoj888 <saunmanoj628@gmail.com>
2020-04-05 16:29:48 +0530

    Devise gem and sacffold property

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :properties
+  devise_for :accounts
+  root to:  'public#main'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
